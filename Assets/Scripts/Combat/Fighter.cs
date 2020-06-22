84e4e64d0e806a5d135e12587642273127dde55d
Longkai Sui <m30466185@gmail.com>
2020-06-22 19:57:49 -0400

    Finish Player's basic attack behavior

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,11 +10,13 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        // Player can't attack without interruption. 
         [SerializeField] float timeBetweenAttacks = 3f;
+        [SerializeField] float weaponDamage = 10f;
 
         float timeSinceLastAttack = 0;
 
-        [SerializeField] Transform target = null;
+        [SerializeField] Health target = null;
         Mover mover;
 
         private void Start()
@@ -26,10 +28,10 @@ namespace RPG.Combat
         {
             timeSinceLastAttack += Time.deltaTime;
             // This script works only works after having an attack target.
-            if (target == null) return;
+            if (target == null || target.IsDead()) return;
             if (!GetIsInRange())
             {
-                mover.MoveTo(target.position);
+                mover.MoveTo(target.transform.position);
             }else
             {
                 mover.Cancel();
@@ -39,45 +41,33 @@ namespace RPG.Combat
 
         private void AttackBehaviour()
         {
+            transform.LookAt(target.transform);
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
-                GetComponent<Animator>().SetTrigger("Attack");
-            }     
+                // This will trigger Hit() event.
+                TriggerAttack();
+                /*
+                    we have finished once attack,
+                    so we should set this variable to 0
+                 */
+                timeSinceLastAttack = 0f;
+            }
         }
 
-        /// <summary>
-        ///
-        /// Whether the target is within player's attack range.
-        /// 
-        /// 
-        /// </summary>
-        /// <returns>
-        /// true: player should stop false: player should move
-        /// </returns>
-        private bool GetIsInRange()
+        private void TriggerAttack()
         {
-            return
-                Vector3.Distance(transform.position, target.position) < weaponRange;
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
-        /// <summary>
-        /// This method is used to change actions from others to attack.
-        /// </summary>
-        /// <param name="combatTarget">Enemy who fight with player</param>
-        public void Attack(CombatTarget combatTarget)
+        public bool CanAttack(CombatTarget combatTarget)
         {
-            GetComponent<ActionScheduler>().StartAction(this);
-            target = combatTarget.transform;
-        }
-
-        /// <summary>
-        /// Implement method from interface.
-        /// 
-        /// Cancel the attack action.
-        /// </summary>
-        public void Cancel()
-        {
-            target = null;
+            if (combatTarget == null)
+            {
+                return false;
+            }
+            Health targetToTest = combatTarget.GetComponent<Health>();
+            return (targetToTest != null) && !targetToTest.IsDead();
         }
 
         /// <summary>
@@ -87,7 +77,56 @@ namespace RPG.Combat
         /// </summary>
         void Hit()
         {
+            if (target == null)
+            {
+                return;
+            }
+            target.TakeDamage(weaponDamage);
+        }
 
+        /// <summary>
+        ///
+        /// Whether the target is within player's attack range.
+        /// 
+        /// </summary>
+        /// <returns>
+        /// true: player should stop false: player should move
+        /// </returns>
+        private bool GetIsInRange()
+        {
+            return
+                Vector3.Distance(transform.position, target.transform.position)
+                < weaponRange;
+        }
+
+        /// <summary>
+        /// 
+        /// This method is used to change actions from others to attack.
+        /// 
+        /// </summary>
+        /// <param name="combatTarget">Enemy who fight with player</param>
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.GetComponent<Health>();
+        }
+
+        /// <summary>
+        /// Implement method from interface.
+        /// 
+        /// Cancel the attack action.
+        /// 
+        /// </summary>
+        public void Cancel()
+        {
+            StopAttack();
+            target = null;
+        }
+
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
         }
     }
 }
---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] float health = 100f;
+
+        bool isDead = false;
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
+        /// <summary>
+        ///
+        /// Decrease character's health.
+        /// 
+        /// </summary>
+        /// <param name="damage">Reduced value</param>
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(health - damage, 0f);
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+
+        /// <summary>
+        ///
+        /// Character's behavior after death.
+        /// 
+        /// </summary>
+        private void Die()
+        {
+            if (isDead)
+            {
+                return;
+            }
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+        }
+    }
+}
